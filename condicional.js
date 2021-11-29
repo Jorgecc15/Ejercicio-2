e11b4b1bf3921595fda3eac914c9c089fc1a11da
Jorgecc15 <92415699+Jorgecc15@users.noreply.github.com>
2021-11-29 18:46:51 -0500

    Update condicional.js

--- condicional.js
+++ condicional.js
@@ -3,7 +3,7 @@ posicion=Number(prompt("A que altura desea llegar? (m)"))
 let t=(0-velocidad)/(0-9.8)
 let desplazamiento=(velocidad*t)-((9.8*t*t)/2)
 if (desplazamiento >= posicion) {
-    respuesta= "el proyectil logra llegar a la altura ;)))))))))"
+    respuesta= "el proyectil logra llegar a la altura"
 } else{
     respuesta= "el proyectil no logra llegar a la altura"
 }
